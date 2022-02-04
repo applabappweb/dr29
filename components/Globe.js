5c03aa663bee91a68d627251b4659dba5358344c
applab appweb <applabappweb@gmail.com>
2022-02-04 17:48:59 +0100

    commit

--- components/Globe.js
+++ components/Globe.js
@@ -4,12 +4,14 @@ import styles from './Globe.module.css'
 export default function Globe() {
   webglGlobe()
   return (
-    <div className={`homeHeroContainer ${styles.flexAuto}`}>
-      <div className={`${styles.heightFull} jsWebglGlobe`}></div>
-      <div className={styles.stars}>
-        <div className={styles.signupBgStars}></div>
-        <div className={styles.signupBgStars2}></div>
-        <div className={styles.signupBgStars3}></div>
+    <div className={styles.dFlex}>
+      <div className={`homeHeroContainer jsWebglGlobeData ${styles.flexAuto}`}>
+        <div className={`${styles.heightFull} jsWebglGlobe`}></div>
+        <div className={styles.stars}>
+          <div className={styles.signupBgStars}></div>
+          <div className={styles.signupBgStars2}></div>
+          <div className={styles.signupBgStars3}></div>
+        </div>
       </div>
     </div>
   )
